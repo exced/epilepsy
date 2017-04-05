38d4afc8f477225376339f73122a0047def82e87
exced <thomas.barras26@gmail.com>
2017-04-05 15:21:18 -0400

    background

--- public/app.js
+++ public/app.js
@@ -2,6 +2,11 @@
 
 var rows = 6;
 var columns = 6;
+var x = 0;
+var y = 0;
+var nbDrops = 50;
+var fallingDrops = [];
+var contextBackground;
 
 var vue = new Vue({
     el: '#timer',
@@ -56,7 +61,8 @@ function WindowLoad(event) {
         spongebob2: "./assets/spongebob2.jpeg",
         spongebob3: "./assets/spongebob3.jpg",
     }
-    animateBackground();
+    var canvasBackground = document.getElementById('background');
+    animateBackground(canvasBackground);
     loadImages(sprites, function (imgs) {
         var canvas = document.getElementById('canvas');
         var game = new Game(imgs, canvas, rows, columns);
@@ -65,10 +71,39 @@ function WindowLoad(event) {
     });
 }
 
+function draw() {
+    for (var i = 0; i < nbDrops; i++) {
+        contextBackground.drawImage(fallingDrops[i].image, fallingDrops[i].x, fallingDrops[i].y, 5, 5); //The rain drop
+
+        fallingDrops[i].y += fallingDrops[i].speed; //Set the falling speed
+        if (fallingDrops[i].y > screen.availHeight) {  //Repeat the raindrop when it falls out of view
+            fallingDrops[i].y = -25 //Account for the image size
+            fallingDrops[i].x = Math.random() * screen.availWidth;    //Make it appear randomly along the width    
+        }
+
+    }
+}
+
 /**
  * 
  */
-function animateBackground() {
+function animateBackground(canvas) {
+    if (canvas.getContext) {
+        contextBackground = canvas.getContext('2d');
+        setInterval(draw, 36);
+        for (var i = 0; i < nbDrops; i++) {
+            var fallingDr = new Object();
+            fallingDr["image"] = new Image();
+            fallingDr.image.src = "./assets/spongebob1.jpg";
+
+            fallingDr["x"] = Math.random() * 600;
+            fallingDr["y"] = Math.random() * 5;
+            fallingDr["speed"] = 3 + Math.random() * 5;
+            fallingDrops.push(fallingDr);
+        }
+
+    }
+
     setInterval(function () {
         $('#animate').css("background-color", "yellow");
         setTimeout(function () {
@@ -87,7 +122,8 @@ function startTimer(duration) {
         vue.timer = minutes + ":" + seconds;
         if (--timer < 0 || vue.ended) {
             clearInterval(t);
-            // end 
+            // I will never end
+            startTimer(duration);
         }
     }, 1000);
 }
